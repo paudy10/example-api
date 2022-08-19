72a02d93299b01ec8c46cd612b20109cbbab7bff
JvdPd <48894529+paudy10@users.noreply.github.com>
2022-08-19 13:50:34 +0430

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose  = require('mongoose');
 
-const MONGOURI = "mongodb+srv://Jvdpd:Jvdpd1021@cluster.fh4ra.mongodb.net/?retryWrites=true&w=majority";
+const MONGOURI = "mongodb+srv://Jvdpd:jvdpd1021@cluster.fh4ra.mongodb.net/appsaz";
 const InitiateMongoServer = async () => {
   try {
     await mongoose.connect(MONGOURI, {
